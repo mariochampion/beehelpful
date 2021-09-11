3edb8625bf4c757f59653d4391b43fff0bd379e0
mariochampion <mario@mariochampion.com>
2021-09-11 15:08:33 -0500

    bare bones to get started

---
+++ beehelpful.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+'''
+take a N number of letters and spin up the permutations and check which are words
+
+'''
+
+## ===================================================================
+## LICENSE AND CREDITS
+## This app/collection of scripts at https://github.com/mariochampion/liquibase-drift-reports
+## released under the Apache License 2.0. (http://www.apache.org/licenses/LICENSE-2.0)
+##
+## Latest Liquibase Release: https://github.com/liquibase/liquibase/releases
+## Contribute code to Liquibase: https://github.com/liquibase/liquibase
+##
+## please open issues and pull requests,
+## thanks and always remember: this robot loves you. 
+## boop boop!
+## ===================================================================
+
+
+
+################################# love your library(s)
+import os, sys
+
+
+
+
+######################################
+## do the initial main work
+def main(args):
+	
+	print("let's get this spelling bee started!")
+
+
+
+
+
+
+
+############## GET STARTED	
+
+#################################
+# boilerplate kicker offer (yes thats a tech term!)   
+if __name__ == '__main__':
+  
+  try:
+    args
+  except:
+    args = sys.argv[1:]
+  
+  main(args)
+
+
+
