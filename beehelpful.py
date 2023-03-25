e344782cec8bd43c5b321fea8414032c59c63ce9
mario champion <mario@mariochampion.com>
2023-03-25 16:37:14 -0500

    needed_letters.issubset(guessed_letters) and other changes

--- beehelpful.py
+++ beehelpful.py
@@ -2,17 +2,14 @@
 # -*- coding: utf-8 -*-   
 
 '''
-take a N number of letters and spin up the permutations and check which are words
+basically a 7 letter hangman in spanish
 
 '''
 
 ## ===================================================================
 ## LICENSE AND CREDITS
-## This app/collection of scripts at https://github.com/mariochampion/liquibase-drift-reports
-## released under the Apache License 2.0. (http://www.apache.org/licenses/LICENSE-2.0)
 ##
-## Latest Liquibase Release: https://github.com/liquibase/liquibase/releases
-## Contribute code to Liquibase: https://github.com/liquibase/liquibase
+## just some tests and tutorials for eliana and chatgpt 
 ##
 ## please open issues and pull requests,
 ## thanks and always remember: this robot loves you. 
@@ -42,17 +39,24 @@ def validate_letter(letter):
 
 def play_game():
     active_word = get_word(spanish_words)
+    needed_letters = set(active_word)
     print("active word: " + active_word)
     print(" ")
     guessed_letters = set()
-    max_guesses = 10
+    max_guesses = 11
     
     while max_guesses > 0:
+        max_guesses -= 1
         print("Adivina una letra de la palabra de " + str(len(active_word)) + " letras:")
         print(" ".join([letter if letter in guessed_letters else "_" for letter in active_word]))
         print(" ")
         
         letter = raw_input("adivina una letra: ").lower()
+        if letter == "99":
+        	print("current guessed letters: ")
+        	print(guessed_letters)
+        	print(needed_letters)
+        	print(" ")
         if not validate_letter(letter):
             continue
         
@@ -64,15 +68,16 @@ def play_game():
         if letter in active_word:
             positions = [i+1 for i, char in enumerate(active_word) if char == letter]
             print("La letra '" + letter + "' está en las palabra " + active_word + ".")
-            if set(active_word) == guessed_letters:
+            if needed_letters.issubset(guessed_letters):
                 print(" ")
                 print("-------!!!------------!!!---------")
                 print("Felicidades! Adivinaste la palabra.")
                 print("-------!!!------------!!!---------")
                 print(" ")
                 return
+        
         else:
-            max_guesses -= 1
+            
             print("Lo siento, la letra '" + letter + "' no está en la palabra. Te quedan "+ str(max_guesses) + " intentos.")
     
     print("Lo siento, no adivinaste la palabra. La palabra era '" + active_word + "'.")
