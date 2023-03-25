2c6b0ca6aa36602d2ec8cdeda47cf12cd16c03d8
mario champion <mario@mariochampion.com>
2023-03-25 15:27:27 -0500

    quick update tweaked from chatgpt proposal
    chaptgpt was close! but not quite!
    from this prompt:
    1. set up a list of 7 letter spanish words
    2. pick one word to the "active" word
    3. ask user for a letter (and tell user "that is not a letter, try again) if their input is any character which is not a letter
    3. if the letter is in the word, tell the user in which position (1, 2, 3, 4, 5, 6, or 7 ) it is in
    4. if the letter is not in the word, tell the user "lo siento, esta letra '<their letter>' no está en la palabra"
    5. then allow the user to return to step 3 (asking them another letter)

--- beehelpful.py
+++ beehelpful.py
@@ -21,23 +21,56 @@ take a N number of letters and spin up the permutations and check which are word
 
 ################################# love your library(s)
 import os, sys
+import random
 
 
 
+def get_word():
+    words = ['abogado', 'basquet', 'canela', 'dorados', 'enfermo', 'futbol', 'guitar']
+    return random.choice(words)
 
-######################################
-## do the initial main work
-def main(args):
-	
-	print("")
-	print("------------------------------------")
-	print("let's get this spelling bee started!")
-	print("------------------------------------")
-	print("")
+def validate_letter(letter):
+    if not letter.isalpha():
+        print("Lo siento, eso no es una letra. Por favor, intenta de nuevo.")
+        return False
+    return True
 
-
-
-
+def play_game():
+    active_word = get_word().lower()
+    print("active word: " + active_word)
+    print(" ")
+    guessed_letters = set()
+    max_guesses = 10
+    
+    while max_guesses > 0:
+        print("Adivina una letra de la palabra de " + str(len(active_word)) + " letras:")
+        print(" ".join([letter if letter in guessed_letters else "_" for letter in active_word]))
+        print(" ")
+        
+        letter = raw_input("adivina una letra: ").lower()
+        if not validate_letter(letter):
+            continue
+        
+        if letter in guessed_letters:
+            print("Ya adivinaste la letra '" + letter + "'. Por favor intenta otra.")
+            continue
+        
+        guessed_letters.add(letter)
+        if letter in active_word:
+            positions = [i+1 for i, char in enumerate(active_word) if char == letter]
+            print("La letra '" + letter + "' esta en las palabra " + active_word + ".")
+            if set(active_word) == guessed_letters:
+                print(" ")
+                print("-------!!!------------!!!---------")
+                print("Felicidades! Adivinaste la palabra.")
+                print("-------!!!------------!!!---------")
+                print(" ")
+                return
+        else:
+            max_guesses -= 1
+            print("Lo siento, la letra '" + letter + "' no esta en la palabra. Te quedan "+ str(max_guesses) + " intentos.")
+    
+    print("Lo siento, no adivinaste la palabra. La palabra era '" + active_word + "'.")
 
 
 
@@ -47,12 +80,6 @@ def main(args):
 # boilerplate kicker offer (yes thats a tech term!)   
 if __name__ == '__main__':
   
-  try:
-    args
-  except:
-    args = sys.argv[1:]
-  
-  main(args)
-
+  play_game()
 
 
